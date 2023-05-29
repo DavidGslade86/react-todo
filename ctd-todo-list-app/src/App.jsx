12fd95c79421e843c7b3f488d4ddc558d758e5a6
David Greenslade <david.greenslade.cohort2211@gmail.com>
2023-05-29 12:20:32 -0700

    lesson 1.1 first commit

---
+++ ctd-todo-list-app/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const todoList = [
+    {id:1,
+    title:"Complete Assignment"},
+    {id:2,
+    title:"Complete Reading"},
+    {id:3,
+    title:"Annotate notes"},
+    {id:4,
+    title:"Watch Videos and update notes"}
+  ]
+
+  return (
+    <>
+      <h1 className="title">Todo List</h1>
+      <ul className="list--items">
+        {todoList.map(item => <li>{item.title}</li>)}
+      </ul>
+    </>
+  )
+}
+
+export default App
